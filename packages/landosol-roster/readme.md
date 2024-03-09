21fa821a5d32ac666c47dbc5188a5b48c4d37ede
SaarChaffee <saarchaffee@qq.com>
2024-03-09 12:17:15 +0800

    feat: local Landosol Roster

---
+++ packages/landosol-roster/readme.md
@@ -0,0 +1,5 @@
+<h1 align="center">PCR 兰德索尔花名册</h1>
+
+[![npm](https://img.shields.io/npm/v/koishi-plugin-pcr-landosol-roster?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-pcr-landosol-roster)
+
+用于 [PCR 核心插件](https://github.com/SaarChaffee/koishi-plugin-pcr/tree/master/packages/pcr) 的兰德索尔花名册数据源，使用本插件后将会从本地调用兰德索尔花名册。
---
+++ packages/landosol-roster/src/index.ts
@@ -0,0 +1,52 @@
+import { Context, Schema, Service } from 'koishi'
+
+export const name = 'landosol-roster'
+
+declare module 'koishi' {
+  interface Context {
+    'landosol-roster': LandosolRoster
+  }
+  interface Events {
+    'pcr/landosol-rester-start'(): void
+    'pcr/landosol-rester-stop'(): void
+  }
+}
+
+export const Config: Schema<object> = Schema.object({})
+
+export class LandosolRoster extends Service {
+  private CHARA_NAME
+  private CHARA_PROFILE
+  private UNAVAILABLE_CHARA: number[]
+
+  constructor(ctx: Context) {
+    super(ctx, 'landosol-roster', true)
+    this.CHARA_NAME = require('../data/chara_name.json')
+    this.CHARA_PROFILE = require('../data/chara_profile.json')
+    this.UNAVAILABLE_CHARA = require('../data/unavailable_chara.json')
+  }
+
+  charaName() {
+    return this.CHARA_NAME
+  }
+
+  charaProfile() {
+    return this.CHARA_PROFILE
+  }
+
+  unavailableChara(): number[] {
+    return this.UNAVAILABLE_CHARA
+  }
+}
+
+export function apply(ctx: Context) {
+  ctx.plugin(LandosolRoster)
+
+  ctx.on('ready', () => {
+    ctx.emit('pcr/landosol-rester-start')
+  })
+
+  ctx.on('dispose', () => {
+    ctx.emit('pcr/landosol-rester-stop')
+  })
+}
